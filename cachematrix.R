62942fec25ed603ee61e4fcd92db1b1622481414
BetsyJ314 <31372601+BetsyJ314@users.noreply.github.com>
2017-09-24 09:33:54 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a cashed matrix to pull into other environments.
 
-## Write a short comment describing this function
+## This function creates the cashed inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m = NULL
+  ##set value of matrix
+  set = function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  ##get value of matrix
+  get = function() x
+  ##set inverse
+  setinverse = function(solve) m <<- solve
+  ##get inverse
+  getinverse = function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function checks to see if a cached inverse matrix exists.
+## If so, bring it in
+## If not, calculate the inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m = x$getinverse()
+  ##if inverse already calc'd, bring it in here
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  ##if not already calc'd, calc it
+  data = x$get()
+  m = solve(data, ...)
+  x$setinverse(m)
+  return(m)
 }
